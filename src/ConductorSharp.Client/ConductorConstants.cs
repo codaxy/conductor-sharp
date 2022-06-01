60420800b2e85e4f6ee76fa0a50427c030ab5f63
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-06-01 14:53:50 +0200

    Update ConductorConstants.cs

--- src/ConductorSharp.Client/ConductorConstants.cs
+++ src/ConductorSharp.Client/ConductorConstants.cs
@@ -5,7 +5,6 @@ namespace ConductorSharp.Client
 {
     public static class ConductorConstants
     {
-        //Test
         public static string SimpleTask { get; } = "SIMPLE";
         public static string SubworkflowTask { get; } = "SUB_WORKFLOW";
         public static JsonSerializer IoJsonSerializer { get; } =
