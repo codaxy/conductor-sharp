ff53e88bafb7c1cc1b77de4488dfd87c872fa3cf
bojan.malinic <bojan.malinic@codaxy.com>
2022-06-15 12:37:08 +0200

    Add marker interface, apply it to relevant models

---
+++ src/ConductorSharp.Engine/Interface/INameable.cs
@@ -0,0 +1,7 @@
+﻿namespace ConductorSharp.Engine.Interface
+{
+    // This interface should be applied to models coresponding to nameable condutctor objects (e.g workfow, subworkflow, simple tasks)
+    public interface INameable
+    {
+    }
+}
--- src/ConductorSharp.Engine/Model/SimpleTaskModel.cs
+++ src/ConductorSharp.Engine/Model/SimpleTaskModel.cs
@@ -1,9 +1,10 @@
-﻿using MediatR;
+﻿using ConductorSharp.Engine.Interface;
+using MediatR;
 
 namespace ConductorSharp.Engine.Model
 {
 
-    public abstract class SimpleTaskModel<I, O> : TaskModel<I, O> where I : IRequest<O>
+    public abstract class SimpleTaskModel<I, O> : TaskModel<I, O>, INameable where I : IRequest<O>
     {
     }
 }
