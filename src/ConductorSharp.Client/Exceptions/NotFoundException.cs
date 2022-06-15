ef5bcff4e65b4c346505441224aa2e29e605a3c0
bojan.malinic <bojan.malinic@codaxy.com>
2022-06-15 14:01:04 +0200

    Add NotFoundException, throw it in case of 404 response from conductor instance

---
+++ src/ConductorSharp.Client/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConductorSharp.Client.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
