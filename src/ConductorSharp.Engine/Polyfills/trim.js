329ca6957bf501149773ed7557dd34aaa602492b
bojan.malinic <bojan.malinic@codaxy.com>
2022-06-21 15:04:35 +0200

    Add trim polyfill

---
+++ src/ConductorSharp.Engine/Polyfills/trim.js
@@ -0,0 +1,25 @@
+﻿(function (str, chars) {
+    let map = {};
+
+    if (typeof chars == 'string')
+        chars = [chars];
+
+    for (let i = 0; i < chars.length; ++i) {
+        map[chars[i]] = true;
+    }
+
+    let front = 0;
+    let back = str.length - 1;
+
+    for (let i = 0; i <= Math.floor(str.length / 2); ++i) {
+        if (map[str[front]])
+            ++front;
+        if (map[str[back]])
+            --back;
+    }
+
+    if (back < front)
+        return "";
+    else
+        return str.substring(front, back + 1);
+})
