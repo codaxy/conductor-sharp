cc245d40858265781b956997f7ba0419b866d5fb
bojan.malinic <bojan.malinic@codaxy.com>
2022-06-25 10:17:17 +0200

    Fix namespaces and usings

---
+++ src/ConductorSharp.Toolkit/Models/WFECommandInput.cs
@@ -0,0 +1,14 @@
+﻿namespace ConductorSharp.Toolkit.Models
+{
+    public class WFECommandInput
+    {
+        public string Action { get; set; }
+        public string Api { get; set; }
+        public string Namespace { get; set; }
+        public string Host { get; set; }
+        public bool Dryrun { get; set; }
+        public string Destination { get; set; }
+        public string YmlDestination { get; set; }
+        public string Source { get; set; }
+    }
+}
