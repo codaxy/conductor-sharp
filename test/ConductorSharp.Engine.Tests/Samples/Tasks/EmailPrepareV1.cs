9cf5ca9ba27f3c7e7185d9746feb1965c9b31385
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-07-01 12:49:18 +0200

    add basic tests (#19)
    * add basic tests
    
    * drop nullable
    
    * add test action
    
    * remove extra space
    
    * drop exclamation

---
+++ test/ConductorSharp.Engine.Tests/Samples/Tasks/EmailPrepareV1.cs
@@ -0,0 +1,29 @@
+﻿namespace ConductorSharp.Engine.Tests.Samples.Tasks;
+
+public partial class EmailPrepareV1Input : IRequest<EmailPrepareV1Output>
+{
+    /// <originalName>
+    /// address
+    /// </originalName>
+    public dynamic Address { get; set; }
+    /// <originalName>
+    /// name
+    /// </originalName>
+    public dynamic Name { get; set; }
+}
+
+public partial class EmailPrepareV1Output
+{
+    /// <originalName>
+    /// email_body
+    /// </originalName>
+    public dynamic EmailBody { get; set; }
+}
+
+/// <originalName>
+/// EMAIL_prepare
+/// </originalName>
+[OriginalName("EMAIL_prepare")]
+public partial class EmailPrepareV1 : SimpleTaskModel<EmailPrepareV1Input, EmailPrepareV1Output>
+{
+}
---
+++ test/ConductorSharp.Engine.Tests/Unit/TaskDefinitionBuilderTests.cs
@@ -0,0 +1,16 @@
+﻿using ConductorSharp.Engine.Tests.Samples.Workers;
+
+namespace ConductorSharp.Engine.Tests.Unit
+{
+    public class TaskDefinitionBuilderTests
+    {
+        [Fact]
+        public void ReturnsCorrectDefinition()
+        {
+            var definition = JsonConvert.SerializeObject(TaskDefinitionBuilder.Build<GetCustomerHandler>(null), Formatting.Indented);
+            var expectedDefinition = EmbeddedFileHelper.GetLinesFromEmbeddedFile("~/Samples/Tasks/CustomerGet.json");
+
+            Assert.Equal(expectedDefinition, definition);
+        }
+    }
+}
