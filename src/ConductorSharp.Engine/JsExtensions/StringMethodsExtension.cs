082f34433d76ee4105e6450ab3fb223c8769cee3
bojan.malinic <bojan.malinic@codaxy.com>
2022-07-19 15:55:04 +0200

    Add support for Remove method, use new extension methods

--- src/ConductorSharp.Engine/JsExtensions/StringMethodsExtension.cs
+++ src/ConductorSharp.Engine/JsExtensions/StringMethodsExtension.cs
@@ -22,47 +22,28 @@ namespace ConductorSharp.Engine.JsExtensions
             switch (expression.Method.Name)
             {
 
-                case nameof(string.Replace):
-                    {
-                        if (@params.Length != 2)
-                            throw new NotSupportedException($"{nameof(string.Replace)} method with more than 2 parameters is not supported");
+                case nameof(string.Replace):          
+                    if (@params.Length != 2)
+                        throw new NotSupportedException($"{nameof(string.Replace)} method with more than 2 parameters is not supported");
 
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        context.Write(".replaceAll");
-                        context.WriteManyIsolated('(', ')', ',', expression.Arguments);
-                    }
+                    context.WriteMethodCall(expression.Object, "replaceAll", expression.Arguments);
                     break;
 
                 case nameof(string.ToLowerInvariant):
-                    {
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        context.Write(".toLowerCase()");
-                    }
+                    context.WriteMethodCall(expression.Object, "toLowerCase", expression.Arguments);
                     break;
 
                 case nameof(string.ToUpperInvariant):
-                    {
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        context.Write(".toUpperCase()");
-                    }
+                    context.WriteMethodCall(expression.Object, "toUpperCase", expression.Arguments);
                     break;
 
                 case nameof(string.Trim):
+                    if (@params.Length == 0)
+                        context.WriteMethodCall(expression.Object, "trim", expression.Arguments);       
+                    else
                     {
-                        if (@params.Length == 0)
-                        {
-                            context.Write(expression.Object);
-                            using var op = context.Operation(JavascriptOperationTypes.Call);
-                            context.Write(".trim()");
-                        }
-                        else
-                        {
-                            using var op = context.Operation(JavascriptOperationTypes.Call);
-                            context.WritePolyfillFunction("trim.js", new[] { expression.Object }.Concat(expression.Arguments));
-                        }
+                        using var op = context.Operation(JavascriptOperationTypes.Call);
+                        context.WritePolyfillFunction("trim.js", new[] { expression.Object }.Concat(expression.Arguments));
                     }
                     break;
 
@@ -89,47 +70,39 @@ namespace ConductorSharp.Engine.JsExtensions
                     break;
 
                 case nameof(string.Substring):
-                    {
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        if (@params.Length == 1)
-                        {
-                            context.Write(".substring");
-                            context.WriteManyIsolated('(', ')', ',', expression.Arguments);
-                        }
-                        else
-                        {
-                            var args = new[] { expression.Arguments[0], Expression.MakeBinary(ExpressionType.Add, expression.Arguments[0], expression.Arguments[1]) };
-                            context.Write(".substring");
-                            context.WriteManyIsolated('(', ')', ',', args);
-                        }
-                    }
+                    if (@params.Length == 1)
+                        context.WriteMethodCall(expression.Object, "substring", expression.Arguments);
+                    else
+                        context.WriteMethodCall(expression.Object, "substring", expression.Arguments[0], Expression.MakeBinary(ExpressionType.Add, expression.Arguments[0], expression.Arguments[1]));
+                    
                     break;
 
                 case nameof(string.StartsWith):
-                    {
-                        if (@params.Length != 1)
-                            throw new NotSupportedException($"{nameof(string.StartsWith)} method with more than one parameters is not supported");
-
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        context.Write(".startsWith");
-                        context.WriteManyIsolated('(', ')', ',', expression.Arguments);
-                    }
+                    if (@params.Length != 1)
+                        throw new NotSupportedException($"{nameof(string.StartsWith)} method with more than one parameters is not supported");
+                    context.WriteMethodCall(expression.Object, "startsWith", expression.Arguments);
                     break;
 
-                case nameof(string.EndsWith):
-                    {
-                        if(@params.Length != 1)
-                            throw new NotSupportedException($"{nameof(string.EndsWith)} method with more than one parameters is not supported");
-
-                        context.Write(expression.Object);
-                        using var op = context.Operation(JavascriptOperationTypes.Call);
-                        context.Write(".endsWith");
-                        context.WriteManyIsolated('(', ')', ',', expression.Arguments);
-                    }
+                case nameof(string.EndsWith):                 
+                    if(@params.Length != 1)
+                        throw new NotSupportedException($"{nameof(string.EndsWith)} method with more than one parameters is not supported");
+                    context.WriteMethodCall(expression.Object, "endsWith", expression.Arguments);
                     break;
 
+                case nameof(string.Remove):  
+                    if(@params.Length == 1)
+                        context.WriteMethodCall(expression.Object, "slice", Expression.Constant(0), expression.Arguments[0]);
+                    else
+                    {
+                        using var addOp = context.Operation(JavascriptOperationTypes.AddSubtract);
+                        {
+                            context.WriteMethodCall(expression.Object, "slice", Expression.Constant(0), expression.Arguments[0]);
+                            context.Write('+');
+                            var startIndex = Expression.MakeBinary(ExpressionType.Add, expression.Arguments[0], expression.Arguments[1]);
+                            context.WriteMethodCall(expression.Object, "slice", startIndex);
+                        }
+                    }
+                    break;
                 default:
                     throw new NotSupportedException($"{expression.Method.Name} not supported");
             }
