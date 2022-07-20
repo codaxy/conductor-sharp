6513d534bcd3700bff4487b97eb08771ad6001d5
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2022-07-20 11:36:46 +0200

    Add toolkit project, make dotnet tool (#18)
    * Add project
    
    * Fix class namespaces and using statements
    
    * Remove yml generator
    
    * Remove unnecessary registrations
    
    * Add template files
    
    * Fix namespaces and usings
    
    * Fix namespaces and usings
    
    * Fix namespaces
    
    * Fix namespaces and usings
    
    * Fix usings
    
    * Fix usings
    
    * Fix namespaces and usings
    
    * Fix namespaces and usings
    
    * Commit project file
    
    * Rename class
    
    * Rename class
    
    * Rename class
    
    * Remove properties not related to scaffolding
    
    * Fix namespaces in template files
    
    * Fix namespaces
    
    * Retrieve host value
    
    * Remove code which assumed that wf name starts with capital letters(e.g. UTIL_ or DB_)
    
    * Pack project as tool
    
    * Align private field names to follow default conventions
    
    * Rename variable
    
    * Remove unnecessary comment
    
    * Remove async Task specifiers from method
    
    * Remove unnecessary comment
    
    * Remove async Task
    
    * Do not await unawaitable methods
    
    * Add warning message when workflow description parsing fails
    
    * Add AppendClassProperty method
    
    * Extract functionality which adds generates class property to separate method
    
    * Remove unnecessary assignment
    
    * Extract append xml comment functionality into separate method
    
    Co-authored-by: Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>

---
+++ src/ConductorSharp.Toolkit/Commands/ScaffoldCommand.cs
@@ -0,0 +1,20 @@
+﻿using ConductorSharp.Toolkit.Models;
+using ConductorSharp.Toolkit.Service;
+
+namespace ConductorSharp.Toolkit.Commands
+{
+    public class ScaffoldCommand : Command
+    {
+        private readonly IScaffoldingService _scaffoldingService;
+        public ScaffoldCommand(IScaffoldingService scaffoldingService)
+        {
+            _scaffoldingService = scaffoldingService;
+        }
+
+        public string GetName() => "scaffold";
+        public async Task Execute(CommandInput input)
+        {
+            await _scaffoldingService.Scaffold();
+        }
+    }
+}
