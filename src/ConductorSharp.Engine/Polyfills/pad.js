1e213a44ce655ba385a175400e52c1c60d5dffce
bojan.malinic <bojan.malinic@codaxy.com>
2022-07-21 13:59:02 +0200

    Add pad polyfill

---
+++ src/ConductorSharp.Engine/Polyfills/pad.js
@@ -0,0 +1,9 @@
+﻿function (str, width, char, mode) {
+
+    if (width <= str.length)
+        return str;
+
+    var padding = char.repeat(width - str.length);
+
+    return (mode == "left" ? padding : "") + str + (mode == "right" ? padding : "");
+}
