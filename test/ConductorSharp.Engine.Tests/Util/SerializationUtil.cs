17138bc3da96976e8eb165239290fa90272cf0c7
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2022-07-28 12:19:35 +0200

    Normalize newlines to \n for serialized definitions during testing (#26)
    Normalize newlines

---
+++ test/ConductorSharp.Engine.Tests/Util/SerializationUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Engine.Tests.Util
+{
+    internal static class SerializationUtil
+    {
+        public static string SerializeObject(object @object) => JsonConvert.SerializeObject(@object, Formatting.Indented).Replace("\r\n", "\n");
+    }
+}
