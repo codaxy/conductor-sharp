259fd3e3a783da7f38876e3ba5ec72d02ecae12c
bojan.malinic <bojan.malinic@codaxy.com>
2022-07-28 12:22:01 +0200

    Add test case

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/StringInterpolation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class StringInterpolationInput : WorkflowInput<StringInterpolationOutput>
+    {
+        public string FirstInput { get; set; }
+        public string SecondInput { get; set; }
+    }
+
+    public class StringInterpolationOutput : WorkflowOutput { }
+
+    [OriginalName("TEST_StringInterpolation")]
+    public class StringInterpolation : Workflow<StringInterpolationInput, StringInterpolationOutput>
+    {
+        public EmailPrepareV1 EmailPrepare { get; set; }
+
+        public override WorkflowDefinition GetDefinition()
+        {
+            var builder = new WorkflowDefinitionBuilder<StringInterpolation>();
+
+            builder.AddTask(
+                wf => wf.EmailPrepare,
+                wf =>
+                    new()
+                    {
+                        Address = $"{wf.WorkflowInput.FirstInput},{wf.WorkflowInput.SecondInput}",
+                        Name = $"Workflow name: {NamingUtil.NameOf<StringInterpolation>()}"
+                    }
+            );
+
+            return builder.Build(opts => opts.Version = 1);
+        }
+    }
+}
