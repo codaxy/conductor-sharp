36e3c350da4579aef15a742a1bf9518df1ac55f9
bojan.malinic <bojan.malinic@codaxy.com>
2022-08-08 00:31:43 +0200

    Add missing hash function

--- src/ConductorSharp.Engine/Extensions/FuncExtensions.cs
+++ src/ConductorSharp.Engine/Extensions/FuncExtensions.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ConductorSharp.Engine.Extensions
 {
     internal static class FuncExtensions
     {
-        public static string GetHash(this Delegate @delegate) => Convert.ToBase64String(@delegate.Method.GetMethodBody().GetILAsByteArray());
+        private static readonly SHA1 _sha1 = SHA1.Create();
+
+        public static string GetHash(this Delegate @delegate) =>
+            Convert.ToBase64String(_sha1.ComputeHash(@delegate.Method.GetMethodBody().GetILAsByteArray())).Replace('+', '-').Replace('/', '_');
     }
 }
