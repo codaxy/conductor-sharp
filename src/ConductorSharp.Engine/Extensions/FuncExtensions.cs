500b4185bb15417cdacf2067a891d37fd6483121
bojan.malinic <bojan.malinic@codaxy.com>
2022-08-07 15:38:42 +0200

    Add extension method for function hashing

---
+++ src/ConductorSharp.Engine/Extensions/FuncExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConductorSharp.Engine.Extensions
+{
+    internal static class FuncExtensions
+    {
+        public static string GetHash(this Delegate @delegate) => Convert.ToBase64String(@delegate.Method.GetMethodBody().GetILAsByteArray());
+    }
+}
