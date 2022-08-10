7e187f3d060e48e5cdec644e9c001b65bc3eebb8
bojan.malinic <bojan.malinic@codaxy.com>
2022-08-10 09:15:08 +0200

    Make class public, modify property mapping

--- src/ConductorSharp.Engine/Util/DynamicRequestHandler.cs
+++ src/ConductorSharp.Engine/Util/DynamicRequestHandler.cs
@@ -8,20 +8,19 @@ using System.Threading.Tasks;
 
 namespace ConductorSharp.Engine.Util
 {
-    internal class DynamicRequestHandler<TRequestProxy, TRequest, TResponse> : IRequestHandler<TRequestProxy, TResponse>, IDynamicHandler
+    public class DynamicRequestHandler<TRequestProxy, TRequest, TResponse> : IRequestHandler<TRequestProxy, TResponse>, IDynamicHandler
         where TRequestProxy : IRequest<TResponse>
         where TRequest : new()
     {
-        public Func<TRequest, TResponse> _lambda;
+        public Func<TRequest, TResponse> _handlerFunc;
 
-        public Task<TResponse> Handle(TRequestProxy request, CancellationToken cancellationToken) => Task.FromResult(_lambda(MapRequest(request)));
+        public Task<TResponse> Handle(TRequestProxy request, CancellationToken cancellationToken) =>
+            Task.FromResult(_handlerFunc(MapRequest(request)));
 
         private TRequest MapRequest(TRequestProxy requestProxy)
         {
-            var proxyProperties = typeof(TRequestProxy).GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.SetProperty);
-            var requestProperties = typeof(TRequest)
-                .GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.SetProperty)
-                .ToDictionary(prop => prop.Name);
+            var proxyProperties = typeof(TRequestProxy).GetProperties().Where(prop => prop.CanRead && prop.CanWrite);
+            var requestProperties = typeof(TRequest).GetProperties().Where(prop => prop.CanRead && prop.CanWrite).ToDictionary(prop => prop.Name);
             var request = new TRequest();
 
             foreach (var proxyProp in proxyProperties)
