0e4429a202493ea52fccf3a315e0b47eaa2470e1
bojan.malinic <bojan.malinic@codaxy.com>
2022-08-13 22:42:06 +0200

    Remove unnecessary class

--- src/ConductorSharp.Toolkit/Models/Configuration.cs
+++ src/ConductorSharp.Toolkit/Models/Configuration.cs
@@ -2,16 +2,10 @@
 {
     public class Configuration
     {
-        public class Header
-        {
-            public string Name { get; set; }
-            public string Value { get; set; }
-        }
-
         public string ApiPath { get; set; }
         public string Namespace { get; set; }
         public string BaseUrl { get; set; }
         public string Destination { get; set; }
-        public Header[] Headers { get; set; }
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
     }
 }
