d26a3a71e83cc226976b575594eee1167f80cb82
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2022-08-16 13:29:47 +0200

    Move class to proper namespace (#36)

--- src/ConductorSharp.Engine/Model/SubWorkflowTaskModel.cs
+++ src/ConductorSharp.Engine/Model/SubWorkflowTaskModel.cs
@@ -1,8 +1,7 @@
 ﻿using ConductorSharp.Engine.Interface;
-using ConductorSharp.Engine.Model;
 using MediatR;
 
-namespace ConductorSharp.Engine.Builders
+namespace ConductorSharp.Engine.Model
 {
     public abstract class SubWorkflowTaskModel<I, O> : TaskModel<I, O>, INameable where I : IRequest<O> { }
 }
