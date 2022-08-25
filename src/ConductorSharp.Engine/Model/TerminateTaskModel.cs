bff9cdf6a684739f018d7b9bc7c026edbcab1bcd
bojan.malinic <bojan.malinic@codaxy.com>
2022-08-25 13:25:15 +0200

    Add terminate task model

---
+++ src/ConductorSharp.Engine/Model/TerminateTaskModel.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace ConductorSharp.Engine.Model
+{
+    // TODO: Replace this with enum
+    public static class TerminationStatus
+    {
+        public const string Completed = "COMPLETED";
+        public const string Failed = "FAILED";
+    }
+
+    public class TerminateTaskInput : IRequest<NoOutput>
+    {
+        public dynamic WorkflowOutput { get; set; }
+        public string TerminationStatus { get; set; }
+    }
+
+    public class TerminateTaskModel : TaskModel<TerminateTaskInput, NoOutput> { }
+}
