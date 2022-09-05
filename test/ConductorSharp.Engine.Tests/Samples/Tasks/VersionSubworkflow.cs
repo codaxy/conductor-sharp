36f18a45aea5f63c131c399f3499c80eb9cbbc65
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2022-09-05 14:42:18 +0200

    Read version attribute from subworkflow models (#45)
    * Add Version attribute
    
    * Add ability to scaffold workflow version
    
    * Specify workflow version when scaffolding
    
    * Fetch workflow version from Version attribute
    
    * Make Version internal
    
    * Add VersionAttributeSubworkflow
    
    * Add test case

---
+++ test/ConductorSharp.Engine.Tests/Samples/Tasks/VersionSubworkflow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Engine.Tests.Samples.Tasks
+{
+    public class VersionSubworkflowInput : IRequest<VersionSubworkflowOutput> { }
+
+    public class VersionSubworkflowOutput { }
+
+    [OriginalName("TEST_subworkflow")]
+    [Version(3)]
+    public class VersionSubworkflow : SubWorkflowTaskModel<VersionSubworkflowInput, VersionSubworkflowOutput> { }
+}
