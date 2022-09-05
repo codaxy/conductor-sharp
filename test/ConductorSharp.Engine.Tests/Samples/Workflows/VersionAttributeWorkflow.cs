81dfe60bf755b9b808ff1bd8f25acb2aaa2e415b
bojan.malinic <bojan.malinic@codaxy.com>
2022-09-05 14:26:53 +0200

    Add VersionAttributeSubworkflow

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/VersionAttributeWorkflow.cs
@@ -0,0 +1,20 @@
+﻿namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class VersionAttributeWorkflowInput : WorkflowInput<VersionAttributeWorkflowOutput> { }
+
+    public class VersionAttributeWorkflowOutput : WorkflowOutput { }
+
+    public class VersionAttributeWorkflow : Workflow<VersionAttributeWorkflowInput, VersionAttributeWorkflowOutput>
+    {
+        public VersionSubworkflow TestSubworkflow { get; set; }
+
+        public override WorkflowDefinition GetDefinition()
+        {
+            var builder = new WorkflowDefinitionBuilder<VersionAttributeWorkflow>();
+
+            builder.AddTask(wf => wf.TestSubworkflow, wf => new());
+
+            return builder.Build(opts => opts.Version = 1);
+        }
+    }
+}
