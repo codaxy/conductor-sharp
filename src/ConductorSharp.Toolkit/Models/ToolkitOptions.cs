944aea83e4d295af0324bc05bf3f41b6b4b603ee
bojan.malinic <bojan.malinic@codaxy.com>
2022-09-06 10:14:50 +0200

    Update help text

--- src/ConductorSharp.Toolkit/Models/ToolkitOptions.cs
+++ src/ConductorSharp.Toolkit/Models/ToolkitOptions.cs
@@ -10,10 +10,10 @@ namespace ConductorSharp.Toolkit.Models
         [Option('n', "name", HelpText = "Specifies names of the tasks and workflows to scaffold")]
         public IEnumerable<string> NameFilters { get; set; }
 
-        [Option('a', "app", HelpText = "Specifies owner apps of the tasks and workflows to scaffold")]
+        [Option('a', "app", HelpText = "Specifies owner apps of the workflows to scaffold")]
         public IEnumerable<string> OwnerAppFilters { get; set; }
 
-        [Option('e', "email", HelpText = "Specifies owner emails of the tasks and workflows to scaffold")]
+        [Option('e', "email", HelpText = "Specifies owner emails of the workflows to scaffold")]
         public IEnumerable<string> OwnerEmailFilters { get; set; }
     }
 }
