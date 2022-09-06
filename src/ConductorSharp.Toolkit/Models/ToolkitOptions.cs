159e04595ca45380dfafd914aecc529375209b9c
bojan.malinic <bojan.malinic@codaxy.com>
2022-09-06 10:23:20 +0200

    Update help text

--- src/ConductorSharp.Toolkit/Models/ToolkitOptions.cs
+++ src/ConductorSharp.Toolkit/Models/ToolkitOptions.cs
@@ -7,13 +7,13 @@ namespace ConductorSharp.Toolkit.Models
         [Option('f', "file", HelpText = "Configuration file", Default = "conductorsharp.yaml")]
         public string ConfigurationFilePath { get; set; }
 
-        [Option('n', "name", HelpText = "Specifies names of the tasks and workflows to scaffold")]
+        [Option('n', "name", HelpText = "Specifies names of the tasks and workflows to scaffold (workflow and task filter)")]
         public IEnumerable<string> NameFilters { get; set; }
 
-        [Option('a', "app", HelpText = "Specifies owner apps of the workflows to scaffold")]
+        [Option('a', "app", HelpText = "Specifies owner apps of the workflows to scaffold (workflow filter)")]
         public IEnumerable<string> OwnerAppFilters { get; set; }
 
-        [Option('e', "email", HelpText = "Specifies owner emails of the workflows to scaffold")]
+        [Option('e', "email", HelpText = "Specifies owner emails of the workflows to scaffold (workflow filter)")]
         public IEnumerable<string> OwnerEmailFilters { get; set; }
     }
 }
