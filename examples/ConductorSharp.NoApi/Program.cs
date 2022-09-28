a02ae1d46d7a7c01c56dfdd346f09ab06e3c253a
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-09-28 12:00:28 +0200

    separate context logging into behavior

--- examples/ConductorSharp.NoApi/Program.cs
+++ examples/ConductorSharp.NoApi/Program.cs
@@ -41,6 +41,7 @@ var builder = Host.CreateDefaultBuilder()
                 )
                 .AddPipelines(pipelines =>
                 {
+                    pipelines.AddContextLogging();
                     pipelines.AddRequestResponseLogging();
                     pipelines.AddValidation();
                 });
--- src/ConductorSharp.Engine/Behaviors/RequestResponseLoggingBehavior.cs
+++ src/ConductorSharp.Engine/Behaviors/RequestResponseLoggingBehavior.cs
@@ -14,46 +14,37 @@ namespace ConductorSharp.Engine.Behaviors
     public class RequestResponseLoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
         private readonly ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> _logger;
-        private readonly ConductorSharpExecutionContext _executionContext;
-        private const string LoggerPropertyName = "ConductorContext";
 
-        public RequestResponseLoggingBehavior(
-            ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> logger,
-            ConductorSharpExecutionContext executionContext
-        )
+        public RequestResponseLoggingBehavior(ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> logger)
         {
             _logger = logger;
-            _executionContext = executionContext;
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            using (LogContext.PushProperty(LoggerPropertyName, _executionContext, true))
+            var stopwatch = new Stopwatch();
+            _logger.LogInformation($"Submitting request {{@{typeof(TRequest).Name}}}", request);
+            stopwatch.Start();
+
+            try
             {
-                var stopwatch = new Stopwatch();
-                _logger.LogInformation($"Submitting request {{@{typeof(TRequest).Name}}}", request);
-                stopwatch.Start();
+                var response = await next();
 
-                try
-                {
-                    var response = await next();
+                stopwatch.Stop();
 
-                    stopwatch.Stop();
+                _logger.LogInformation(
+                    $"Received response {{@{typeof(TResponse).Name}}} in {{ellapsedMilliseconds}}",
+                    response,
+                    stopwatch.ElapsedMilliseconds
+                );
 
-                    _logger.LogInformation(
-                        $"Received response {{@{typeof(TResponse).Name}}} in {{ellapsedMilliseconds}}",
-                        response,
-                        stopwatch.ElapsedMilliseconds
-                    );
-
-                    return response;
-                }
-                catch (Exception exc)
-                {
-                    stopwatch.Stop();
-                    _logger.LogInformation($"Exception {{exceptionMessage}} in {{elapsedMilliseconds}}", exc.Message, stopwatch.ElapsedMilliseconds);
-                    throw;
-                }
+                return response;
+            }
+            catch (Exception exc)
+            {
+                stopwatch.Stop();
+                _logger.LogInformation($"Exception {{exceptionMessage}} in {{elapsedMilliseconds}}", exc.Message, stopwatch.ElapsedMilliseconds);
+                throw;
             }
         }
     }
