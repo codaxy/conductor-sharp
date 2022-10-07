606cb6840fd2fd7ee1d1e067c55d4d116414804a
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-10-07 14:23:52 +0200

    Dynamic polling (#60)
    * add strategies
    
    * add missing instantiation
    
    * take into account limit
    
    * recover based on time interval
    
    * remove logging

---
+++ src/ConductorSharp.Engine/Polling/InverseExponentialBackoff.cs
@@ -0,0 +1,36 @@
+﻿using ConductorSharp.Engine.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConductorSharp.Engine.Polling
+{
+    public class InverseExponentialBackoff : IPollTimingStrategy
+    {
+        private const int _backoffRatio = 2;
+        private const int _recoveryValue = 50;
+        private readonly TimeSpan _recoveryInterval = TimeSpan.FromMilliseconds(5000);
+
+        private DateTimeOffset _lastRecoveryTime = DateTimeOffset.UtcNow;
+
+        public int CalculateDelay(
+            IDictionary<string, int> taskQueue,
+            List<TaskToWorker> taskToWorkerList,
+            int baseSleepInterval,
+            int currentSleepInterval
+        )
+        {
+            if (taskToWorkerList.Count > 0)
+            {
+                currentSleepInterval /= _backoffRatio;
+            }
+            else if (DateTimeOffset.UtcNow - _lastRecoveryTime > _recoveryInterval)
+            {
+                currentSleepInterval += 100;
+                _lastRecoveryTime = DateTimeOffset.UtcNow;
+            }
+
+            return Math.Clamp(currentSleepInterval, _recoveryValue, baseSleepInterval);
+        }
+    }
+}
