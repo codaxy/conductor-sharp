093ad91f9de0cbe649253d6ab93f9fc89e40aa7b
bojan.malinic <bojan.malinic@codaxy.com>
2022-10-13 22:10:35 +0200

    Add CSharpLambda class

---
+++ src/ConductorSharp.Engine/Util/CSharpLambda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConductorSharp.Engine.Util
+{
+    public class CSharpLambda
+    {
+        public CSharpLambda(string lambdaIdentifier, Delegate handler, Type inputType)
+        {
+            LambdaIdentifier = lambdaIdentifier;
+            Handler = handler;
+            InputType = inputType;
+        }
+
+        public string LambdaIdentifier { get; }
+        public Delegate Handler { get; }
+        public Type InputType { get; }
+    }
+}
