ee911337eba18c57369d79ee1a30dddbd1259139
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-10-18 14:08:33 +0200

    add read tasks pattern

---
+++ src/ConductorSharp.Patterns/Tasks/ReadContextTasks.cs
@@ -0,0 +1,88 @@
+﻿using ConductorSharp.Client.Service;
+using ConductorSharp.Engine;
+using ConductorSharp.Engine.Util;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Patterns.Tasks
+{
+    public class ReadWorkflowTasksInput : IRequest<ReadWorkflowTasksOutput>
+    {
+        [Required]
+        public string TaskNames { get; set; }
+
+        [Required]
+        public string WorkflowId { get; set; }
+    }
+
+    public class ReadWorkflowTasksOutput
+    {
+        public JObject Tasks { get; set; }
+    }
+
+    public class TaskExecutionDetails
+    {
+        public JObject InputData { get; set; }
+        public JObject OutputData { get; set; }
+        public string ReferenceTaskName { get; set; }
+        public string Status { get; set; }
+    }
+
+    [OriginalName("CSH_PATTERNS_read_tasks")]
+    public class ReadWorkflowTasks : TaskRequestHandler<ReadWorkflowTasksInput, ReadWorkflowTasksOutput>
+    {
+        private readonly ILogger<ReadWorkflowTasks> _logger;
+        private readonly IWorkflowService _workflowService;
+
+        public ReadWorkflowTasks(ILogger<ReadWorkflowTasks> logger, IWorkflowService workflowService)
+        {
+            _logger = logger;
+            _workflowService = workflowService;
+        }
+
+        public async override Task<ReadWorkflowTasksOutput> Handle(ReadWorkflowTasksInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(input.TaskNames))
+                throw new Exception("No task names provided. Comma separated list of reference names expected");
+
+            var tasknames = input.TaskNames.Split(",").Where(a => !string.IsNullOrEmpty(a)).ToList();
+
+            var output = new ReadWorkflowTasksOutput { Tasks = new JObject() };
+
+            var taskNotFoundPrototype = new TaskExecutionDetails
+            {
+                InputData = new JObject(),
+                OutputData = new JObject(),
+                Status = "NOT_FOUND"
+            };
+
+            var starterWorkfow = await _workflowService.GetWorkflowStatus(input.WorkflowId);
+
+            if (starterWorkfow == null)
+                throw new Exception($"Could not find starter workflow by id {input.WorkflowId}");
+
+            var taskData = starterWorkfow.SelectToken("tasks").ToObject<List<TaskExecutionDetails>>();
+
+            foreach (var task in tasknames)
+            {
+                var value =
+                    taskData.Where(a => a.ReferenceTaskName == task).Select(a => JObject.FromObject(a)).FirstOrDefault()
+                    ?? JObject.FromObject(taskNotFoundPrototype);
+                output.Tasks.Add(new JProperty(task, value));
+            }
+
+            // Add workflow input as it might also be useful
+            output.Tasks.Add(new JProperty("workflow", starterWorkfow.SelectToken("input") as JObject));
+
+            return output;
+        }
+    }
+}
