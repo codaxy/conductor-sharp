32c652004705d218d4ec6a3e29c869fb0ee17fcb
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-11-30 19:29:51 +0100

    update workflow def

--- test/ConductorSharp.Engine.Tests/Samples/Workflows/StringInterpolation.cs
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/StringInterpolation.cs
@@ -22,23 +22,21 @@ namespace ConductorSharp.Engine.Tests.Samples.Workflows
     [OriginalName("TEST_StringInterpolation")]
     public class StringInterpolation : Workflow<StringInterpolation, StringInterpolationInput, StringInterpolationOutput>
     {
-        public StringInterpolation(BuildConfiguration buildConfiguration, BuildContext buildContext) : base(buildConfiguration, buildContext) { }
-
         public EmailPrepareV1 EmailPrepare { get; set; }
 
-        public override void SetTasks(WorkflowDefinitionBuilder<StringInterpolation, StringInterpolationInput, StringInterpolationOutput> builder)
+        public override void BuildDefinition()
         {
-            //_builder.AddTask(
-            //    wf => wf.EmailPrepare,
-            //    wf =>
-            //        new()
-            //        {
-            //            Address = $"{wf.WorkflowInput.FirstInput},{wf.WorkflowInput.SecondInput}",
-            //            Name = $"Workflow name: {NamingUtil.NameOf<StringInterpolation>()}"
-            //        }
-            //);
+            _builder.AddTask(
+                wf => wf.EmailPrepare,
+                wf =>
+                    new()
+                    {
+                        Address = $"{wf.WorkflowInput.FirstInput},{wf.WorkflowInput.SecondInput}",
+                        Name = $"Workflow name: {NamingUtil.NameOf<StringInterpolation>()}"
+                    }
+            );
 
-            builder.SetOutput(a => new() { EmailBody = a.EmailPrepare.Output.EmailBody });
+            _builder.SetOutput(a => new() { EmailBody = a.EmailPrepare.Output.EmailBody });
         }
     }
 }
