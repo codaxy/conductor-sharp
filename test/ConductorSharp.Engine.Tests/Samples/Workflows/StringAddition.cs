3902d475bffbacc1c9374ec1c6a8ec8096a1f97e
bojan.malinic <bojan.malinic@codaxy.com>
2022-12-17 17:49:30 +0100

    Add support for string addition

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/StringAddition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class StringAdditionInput : WorkflowInput<StringAdditionOutput>
+    {
+        public string Input { get; set; }
+    }
+
+    public class StringAdditionOutput : WorkflowOutput { }
+
+    [OriginalName("string_addition")]
+    public class StringAddition : Workflow<StringAdditionInput, StringAdditionOutput>
+    {
+        public class StringTaskOutput { }
+
+        public class StringTaskInput : IRequest<StringTaskOutput>
+        {
+            public string Input { get; set; }
+        }
+
+        public LambdaTaskModel<StringTaskInput, StringTaskOutput> StringTask { get; set; }
+
+        public override WorkflowDefinition GetDefinition()
+        {
+            var builder = new WorkflowDefinitionBuilder<StringAddition>();
+
+            builder.AddTask(
+                wf => wf.StringTask,
+                wf =>
+                    new()
+                    {
+                        Input =
+                            1
+                            + "Str_"
+                            + "2Str_"
+                            + wf.WorkflowInput.Input
+                            + $"My input: {wf.WorkflowInput.Input}"
+                            + NamingUtil.NameOf<StringAddition>()
+                            + 1
+                    },
+                ""
+            );
+
+            return builder.Build(opts =>
+            {
+                opts.Version = 1;
+            });
+        }
+    }
+}
