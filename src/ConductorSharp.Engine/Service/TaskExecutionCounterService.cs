db47c1502e4074a0a135e02192ccec6791c654b6
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-12-25 19:10:28 +0100

    add counter service

---
+++ src/ConductorSharp.Engine/Service/TaskExecutionCounterService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConductorSharp.Engine.Service
+{
+    public class TrackedTask
+    {
+        public string TaskId { get; set; }
+        public string TaskName { get; set; }
+        public DateTimeOffset StartedAt { get; set; }
+        public DateTimeOffset CompletedAt { get; set; }
+        public DateTimeOffset FailedAt { get; set; }
+    }
+
+    public class TaskExecutionCounterService
+    {
+        private ConcurrentDictionary<string, TrackedTask> InProgress { get; set; } = new();
+        private ConcurrentDictionary<string, int> Failed { get; set; } = new();
+        private ConcurrentDictionary<string, int> Completed { get; set; } = new();
+
+        public void Track(TrackedTask trackedTask)
+        {
+            InProgress.TryAdd(trackedTask.TaskId, trackedTask);
+        }
+
+        public void MoveToFailed(string taskId)
+        {
+            if (!InProgress.TryGetValue(taskId, out var task))
+            {
+                return;
+            }
+
+            InProgress.Remove(task.TaskId, out var removedTask);
+            Failed.AddOrUpdate(task.TaskName, 0, (id, count) => count++);
+        }
+
+        public void MoveToCompleted(string taskId)
+        {
+            if (!InProgress.TryGetValue(taskId, out var task))
+            {
+                return;
+            }
+
+            InProgress.Remove(task.TaskId, out var removedTask);
+            Completed.AddOrUpdate(task.TaskName, 0, (id, count) => count++);
+        }
+    }
+}
