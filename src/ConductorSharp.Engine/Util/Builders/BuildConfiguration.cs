e1559616ba465600dee6b77150e2f4a78151e63f
bojan.malinic <bojan.malinic@codaxy.com>
2023-02-08 13:56:14 +0100

    Remove unnecessary property

--- src/ConductorSharp.Engine/Util/Builders/BuildConfiguration.cs
+++ src/ConductorSharp.Engine/Util/Builders/BuildConfiguration.cs
@@ -9,6 +9,5 @@ namespace ConductorSharp.Engine.Util.Builders
         public string DefaultOwnerEmail { get; set; }
         public string DefaultOwnerApp { get; set; }
         public string WorkflowPrefix { get; set; }
-        public string LambdaTaskPrefix { get; set; }
     }
 }
