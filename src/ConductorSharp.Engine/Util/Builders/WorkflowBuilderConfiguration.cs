0245bfee76012c166bbfc1e8d3ddd911a5ec7c85
bojan.malinic <bojan.malinic@codaxy.com>
2023-02-08 11:26:18 +0100

    Add WorkflowBuilderConfiguration class

---
+++ src/ConductorSharp.Engine/Util/Builders/WorkflowBuilderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConductorSharp.Engine.Util.Builders
+{
+    public class WorkflowBuilderConfiguration
+    {
+        internal WorkflowBuilderConfiguration(BuildConfiguration buildConfiguration, IEnumerable<ConfigurationProperty> configurationProperties)
+        {
+            BuildConfiguration = buildConfiguration;
+            ConfigurationProperties = configurationProperties;
+        }
+
+        public BuildConfiguration BuildConfiguration { get; }
+
+        public IEnumerable<ConfigurationProperty> ConfigurationProperties { get; }
+    }
+}
