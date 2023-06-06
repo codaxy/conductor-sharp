7ff0d3fdbdc70d160b746d6b21a98605852ecf63
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2023-06-06 09:06:40 +0200

    Bump project versions (#109)

--- src/ConductorSharp.Toolkit/Program.cs
+++ src/ConductorSharp.Toolkit/Program.cs
@@ -16,7 +16,7 @@ namespace ConductorSharp.Toolkit
 {
     class Program
     {
-        public const string Version = "2.0.0-alpha2";
+        public const string Version = "2.0.0";
 
         public async static Task Main(string[] args)
         {
