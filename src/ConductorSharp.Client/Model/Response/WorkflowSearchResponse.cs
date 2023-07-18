e642880351ae6aeda3f6e4ca0dc4a380a694af8c
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-07-18 11:32:08 +0200

    align search response

--- src/ConductorSharp.Client/Model/Response/WorkflowSearchResponse.cs
+++ src/ConductorSharp.Client/Model/Response/WorkflowSearchResponse.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using ConductorSharp.Client.Model.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +9,34 @@ namespace ConductorSharp.Client.Model.Response
 {
     public class WorkflowSearchResponse
     {
+        public class Data
+        {
+            public string WorkflowType { get; set; }
+            public int Version { get; set; }
+            public string WorkflowId { get; set; }
+            public string CorrelationId { get; set; }
+            public DateTimeOffset StartTime { get; set; }
+            public DateTimeOffset UpdateTime { get; set; }
+            public DateTimeOffset EndTime { get; set; }
+            public string Status { get; set; }
+            public string Input { get; set; }
+            public string Output { get; set; }
+            public string ReasonForIncompletion { get; set; }
+            public long ExecutionTime { get; set; }
+            public string Event { get; set; }
+            public string FailedReferenceTaskNames { get; set; }
+            public string ExternalInputPayloadStoragePath { get; set; }
+            public string ExternalOutputPayloadStoragePath { get; set; }
+            public int Priority { get; set; }
+            public List<string> FailedTaskNames { get; set; } = new List<string>();
+            public long OutputSize { get; set; }
+            public long InputSize { get; set; }
+        }
+
         [JsonProperty("totalHits")]
         public int TotalHits { get; set; }
 
         [JsonProperty("results")]
-        public List<WorkflowStatusResponse> Results { get; set; }
+        public List<Data> Results { get; set; }
     }
 }
