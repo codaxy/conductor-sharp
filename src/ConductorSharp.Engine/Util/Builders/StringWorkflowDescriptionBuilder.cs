88a54d6e4d7167cfc2a6fb0026e72a0a8338ce35
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-07-28 13:52:35 +0200

    make workflow description formatting configurable

---
+++ src/ConductorSharp.Engine/Util/Builders/StringWorkflowDescriptionBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace ConductorSharp.Engine.Util.Builders
+{
+    public class StringWorkflowDescriptionBuilder : IWorkflowDescriptionBuilder
+    {
+        public string Build(BuildContext context) => context.WorkflowOptions.Description;
+    }
+}
