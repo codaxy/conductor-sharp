b42571606b758a19fc084ea5ed691287a3facf9e
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2023-09-29 16:07:02 +0200

    Register workflow for every test case, add missing test case (#156)
    * Register workflow for every test case
    
    * Add missing test case
    
    * Commit project file

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/ListInitializationWorkflow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class ListInitializationWorkflowInput : WorkflowInput<ListInitializationWorkflowOutput> { }
+
+    public class ListInitializationWorkflowOutput : WorkflowOutput { }
+
+    public class ListInitializationWorkflow : Workflow<ListInitializationWorkflow, ListInitializationWorkflowInput, ListInitializationWorkflowOutput>
+    {
+        public ListTask ListTask { get; set; }
+
+        public ListInitializationWorkflow(
+            WorkflowDefinitionBuilder<ListInitializationWorkflow, ListInitializationWorkflowInput, ListInitializationWorkflowOutput> builder
+        ) : base(builder) { }
+
+        public override void BuildDefinition()
+        {
+            _builder.AddTask(
+                wf => wf.ListTask,
+                wf =>
+                    new()
+                    {
+                        List = new()
+                        {
+                            1,
+                            "str",
+                            new { TestProp = "testProp" },
+                            new NestedObjects.TestModel { Integer = 1 }
+                        }
+                    }
+            );
+        }
+    }
+}
