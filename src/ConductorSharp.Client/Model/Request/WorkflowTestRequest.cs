80000f2624e53eeb8c87cecf9eb3007622228c98
bojan.malinic <bojan.malinic@codaxy.com>
2023-10-09 15:47:40 +0200

    Fix request type

--- src/ConductorSharp.Client/Model/Request/WorkflowTestRequest.cs
+++ src/ConductorSharp.Client/Model/Request/WorkflowTestRequest.cs
@@ -1,60 +1,30 @@
 ﻿using ConductorSharp.Client.Model.Common;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace ConductorSharp.Client.Model.Request
 {
-    public partial class WorkflowTestRequest
+    public class WorkflowTestRequest
     {
-        public partial class TaskMock
+        public class TaskMock
         {
-            public enum TaskMockStatus
-            {
-                [System.Runtime.Serialization.EnumMember(Value = @"IN_PROGRESS")]
-                IN_PROGRESS = 0,
-
-                [System.Runtime.Serialization.EnumMember(Value = @"FAILED")]
-                FAILED = 1,
-
-                [System.Runtime.Serialization.EnumMember(Value = @"FAILED_WITH_TERMINAL_ERROR")]
-                FAILED_WITH_TERMINAL_ERROR = 2,
-
-                [System.Runtime.Serialization.EnumMember(Value = @"COMPLETED")]
-                COMPLETED = 3,
-            }
-
-            public TaskMockStatus Status { get; set; }
-            Dictionary<string, object> Output { get; set; }
-
+            public string Status { get; set; }
+            public JObject Output { get; set; }
             public long ExecutionTime { get; set; }
-
             public long QueueWaitTime { get; set; }
-
-            public Dictionary<string, object> AdditionalProperties { get; set; } = new();
+            public JObject AdditionalProperties { get; set; }
         }
 
         public string Name { get; set; }
-
         public int Version { get; set; }
-
         public string CorrelationId { get; set; }
-
-        public Dictionary<string, object> Input { get; set; }
-
+        public JObject Input { get; set; }
         public Dictionary<string, string> TaskToDomain { get; set; }
-
         public WorkflowDefinition WorkflowDef { get; set; }
-
         public string ExternalInputPayloadStoragePath { get; set; }
-
         public int Priority { get; set; }
-
-        public Dictionary<string, Collection<TaskMock>> TaskRefToMockOutput { get; set; }
-
+        public Dictionary<string, List<TaskMock>> TaskRefToMockOutput { get; set; }
         public Dictionary<string, WorkflowTestRequest> SubWorkflowTestRequest { get; set; }
-
-        public Dictionary<string, object> AdditionalProperties { get; set; } = new();
+        public JObject AdditionalProperties { get; set; }
     }
 }
