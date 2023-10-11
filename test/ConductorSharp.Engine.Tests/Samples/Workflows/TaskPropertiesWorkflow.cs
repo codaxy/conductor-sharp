bd88ac9d284d73aa0487152dd15aad8b1a7eaab1
bojan.malinic <bojan.malinic@codaxy.com>
2023-10-11 13:44:02 +0200

    Add support for more task properties

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/TaskPropertiesWorkflow.cs
@@ -0,0 +1,36 @@
+﻿namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class TaskPropertiesWorkflowInput : WorkflowInput<TaskPropertiesWorkflowOutput> { }
+
+    public class TaskPropertiesWorkflowOutput : WorkflowOutput { }
+
+    public class TaskPropertiesWorkflow : Workflow<TaskPropertiesWorkflow, TaskPropertiesWorkflowInput, TaskPropertiesWorkflowOutput>
+    {
+        public CustomerGetV1 GetCustomer { get; set; }
+        public TaskPropertiesTask TaskProperties { get; set; }
+
+        public TaskPropertiesWorkflow(
+            WorkflowDefinitionBuilder<TaskPropertiesWorkflow, TaskPropertiesWorkflowInput, TaskPropertiesWorkflowOutput> builder
+        ) : base(builder) { }
+
+        public override void BuildDefinition()
+        {
+            _builder.AddTask(wf => wf.GetCustomer, wf => new());
+            _builder.AddTask(
+                wf => wf.TaskProperties,
+                wf =>
+                    new()
+                    {
+                        Status = wf.GetCustomer.Status,
+                        ReferenceTaskName = wf.GetCustomer.ReferenceTaskName,
+                        StartTime = wf.GetCustomer.StartTime,
+                        EndTime = wf.GetCustomer.EndTime,
+                        ScheduledTime = wf.GetCustomer.ScheduledTime,
+                        TaskDefName = wf.GetCustomer.TaskDefName,
+                        TaskId = wf.GetCustomer.TaskId,
+                        TaskType = wf.GetCustomer.TaskType
+                    }
+            );
+        }
+    }
+}
