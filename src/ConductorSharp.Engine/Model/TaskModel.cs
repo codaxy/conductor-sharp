fa0ccc8c418c3e883720c92495beb9c65954b3de
bojan.malinic <bojan.malinic@codaxy.com>
2023-10-11 13:07:44 +0200

    Add support for more task properties

--- src/ConductorSharp.Engine/Model/TaskModel.cs
+++ src/ConductorSharp.Engine/Model/TaskModel.cs
@@ -1,10 +1,37 @@
-﻿using MediatR;
+﻿using ConductorSharp.Engine.Interface;
+using MediatR;
+using Newtonsoft.Json;
 
 namespace ConductorSharp.Engine.Model
 {
-    public abstract class TaskModel<I, O> where I : IRequest<O>
+    public abstract class TaskModel<I, O> : ITaskModel where I : IRequest<O>
     {
+        // TODO: Remove set
         public I Input { get; set; }
+
         public O Output { get; set; }
+
+        public string Status { get; }
+
+        [JsonProperty("taskType")]
+        public string TaskType { get; }
+
+        [JsonProperty("startTime")]
+        public long StartTime { get; }
+
+        [JsonProperty("endTime")]
+        public long EndTime { get; }
+
+        [JsonProperty("taskId")]
+        public string TaskId { get; }
+
+        [JsonProperty("taskDefName")]
+        public string TaskDefName { get; set; }
+
+        [JsonProperty("scheduledTime")]
+        public long ScheduledTime { get; set; }
+
+        [JsonProperty("referenceTaskName")]
+        public string ReferenceTaskName { get; set; }
     }
 }
