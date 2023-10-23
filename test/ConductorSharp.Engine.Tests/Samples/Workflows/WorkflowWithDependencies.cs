04cf13f536f5939172001620ad5a88fd7ac1d4d2
bojan.malinic <bojan.malinic@codaxy.com>
2023-10-23 15:18:30 +0200

    Modify test case

--- test/ConductorSharp.Engine.Tests/Samples/Workflows/WorkflowWithDependencies.cs
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/WorkflowWithDependencies.cs
@@ -10,6 +10,8 @@ namespace ConductorSharp.Engine.Tests.Samples.Workflows
     {
         private readonly IConfigurationService _testService;
 
+        public CustomerGetV1 GetCustomer { get; set; }
+
         public WorkflowWithDependencies(
             IConfigurationService testService,
             WorkflowDefinitionBuilder<WorkflowWithDependencies, WorkflowWithDependenciesInput, WorkflowWithDependenciesOutput> builder
@@ -20,7 +22,7 @@ namespace ConductorSharp.Engine.Tests.Samples.Workflows
 
         public override void BuildDefinition()
         {
-            _b
+            _builder.AddTask(wf => wf.GetCustomer, wf => new() { CustomerId = _testService.GetValue<int>("CustomerId") });
         }
     }
 }
