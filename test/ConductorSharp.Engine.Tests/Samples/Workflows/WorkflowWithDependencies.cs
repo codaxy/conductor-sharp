72948c12138835c55d81e29eeaaa8b5de8b47ce7
bojan.malinic <bojan.malinic@codaxy.com>
2023-10-23 15:19:17 +0200

    Rename variables

--- test/ConductorSharp.Engine.Tests/Samples/Workflows/WorkflowWithDependencies.cs
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/WorkflowWithDependencies.cs
@@ -8,21 +8,21 @@ namespace ConductorSharp.Engine.Tests.Samples.Workflows
 
     public class WorkflowWithDependencies : Workflow<WorkflowWithDependencies, WorkflowWithDependenciesInput, WorkflowWithDependenciesOutput>
     {
-        private readonly IConfigurationService _testService;
+        private readonly IConfigurationService _configurationService;
 
         public CustomerGetV1 GetCustomer { get; set; }
 
         public WorkflowWithDependencies(
-            IConfigurationService testService,
+            IConfigurationService configurationService,
             WorkflowDefinitionBuilder<WorkflowWithDependencies, WorkflowWithDependenciesInput, WorkflowWithDependenciesOutput> builder
         ) : base(builder)
         {
-            _testService = testService;
+            _configurationService = configurationService;
         }
 
         public override void BuildDefinition()
         {
-            _builder.AddTask(wf => wf.GetCustomer, wf => new() { CustomerId = _testService.GetValue<int>("CustomerId") });
+            _builder.AddTask(wf => wf.GetCustomer, wf => new() { CustomerId = _configurationService.GetValue<int>("CustomerId") });
         }
     }
 }
