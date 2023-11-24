ab6fb3feb93e2d48a255b8547980b64e0f47b5bc
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2023-11-24 10:04:43 +0100

    Add an ability to inject services into workflow (#167)
    * Add an ability to resolve dependencies from workflow
    
    * Rename service class
    
    * Modify test case
    
    * Rename variables

---
+++ test/ConductorSharp.Engine.Tests/Samples/Workflows/WorkflowWithDependencies.cs
@@ -0,0 +1,28 @@
+﻿using ConductorSharp.Engine.Tests.Util;
+
+namespace ConductorSharp.Engine.Tests.Samples.Workflows
+{
+    public class WorkflowWithDependenciesInput : WorkflowInput<WorkflowWithDependenciesOutput> { }
+
+    public class WorkflowWithDependenciesOutput : WorkflowOutput { }
+
+    public class WorkflowWithDependencies : Workflow<WorkflowWithDependencies, WorkflowWithDependenciesInput, WorkflowWithDependenciesOutput>
+    {
+        private readonly IConfigurationService _configurationService;
+
+        public CustomerGetV1 GetCustomer { get; set; }
+
+        public WorkflowWithDependencies(
+            IConfigurationService configurationService,
+            WorkflowDefinitionBuilder<WorkflowWithDependencies, WorkflowWithDependenciesInput, WorkflowWithDependenciesOutput> builder
+        ) : base(builder)
+        {
+            _configurationService = configurationService;
+        }
+
+        public override void BuildDefinition()
+        {
+            _builder.AddTask(wf => wf.GetCustomer, wf => new() { CustomerId = _configurationService.GetValue<int>("CustomerId") });
+        }
+    }
+}
---
+++ test/ConductorSharp.Engine.Tests/Util/ConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿namespace ConductorSharp.Engine.Tests.Util
+{
+    public interface IConfigurationService
+    {
+        T GetValue<T>(string key);
+    }
+
+    public class ConfigurationService : IConfigurationService
+    {
+        public T GetValue<T>(string key) => default;
+    }
+}
