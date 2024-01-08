3d7656e0e1d670bcb68f78401340efc731eda3f0
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2024-01-08 16:08:16 +0100

    tidy up nulls

--- src/ConductorSharp.Client/Util/JsonNodeConverter.cs
+++ src/ConductorSharp.Client/Util/JsonNodeConverter.cs
@@ -1,19 +1,25 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Text.Json.Nodes;
 
 namespace ConductorSharp.Client.Util
 {
     public class JsonNodeConverter : JsonConverter<JsonNode>
     {
-        public override void WriteJson(JsonWriter writer, JsonNode value, JsonSerializer serializer) =>
-            serializer.Serialize(writer, JToken.Parse(value.ToJsonString()));
+        public override void WriteJson(JsonWriter writer, JsonNode? value, JsonSerializer serializer)
+        {
+            if (value is null)
+            {
+                return;
+            }
 
-        public override JsonNode ReadJson(
+            serializer.Serialize(writer, JToken.Parse(value.ToJsonString()));
+        }
+
+        public override JsonNode? ReadJson(
             JsonReader reader,
             Type objectType,
-            JsonNode existingValue,
+            JsonNode? existingValue,
             bool hasExistingValue,
             JsonSerializer serializer
         ) => JsonNode.Parse(JToken.Load(reader).ToString(Formatting.None));
