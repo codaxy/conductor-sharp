5ebf00a0cf54a1e807e03b5e263ecefea482fff6
bojan.malinic <bojan.malinic@codaxy.com>
2024-01-23 13:16:43 +0100

    Remove unnecessary classes

--- src/ConductorSharp.Engine/Util/DocumentationConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConductorSharp.Engine.Util
-{
-    public class DocumentationConfiguration
-    {
-        public string Destination { get; set; }
-        public string Source { get; set; }
-        public string YmlDestination { get; set; }
-    }
-}
