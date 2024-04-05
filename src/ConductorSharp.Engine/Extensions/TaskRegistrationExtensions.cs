7d6fc44792953c24bdd7f78906e0502123453701
Bojan Malinić <94437690+boma96@users.noreply.github.com>
2024-04-05 12:43:39 +0200

    Remove unnecessary registration argument (#184)
    Remove unnecessary argument

--- src/ConductorSharp.Engine/Extensions/TaskRegistrationExtensions.cs
+++ src/ConductorSharp.Engine/Extensions/TaskRegistrationExtensions.cs
@@ -1,42 +1,27 @@
+using System;
 using ConductorSharp.Engine.Builders;
 using ConductorSharp.Engine.Interface;
 using ConductorSharp.Engine.Model;
 using ConductorSharp.Engine.Util.Builders;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace ConductorSharp.Engine.Extensions
 {
     public static class TaskRegistrationExtensions
     {
-        public static void RegisterWorkerTask<TWorkerTask>(
-            this IServiceCollection builder,
-            Action<TaskDefinitionOptions> updateOptions = null,
-            BuildConfiguration buildConfiguration = null
-        ) where TWorkerTask : IWorker
+        public static void RegisterWorkerTask<TWorkerTask>(this IServiceCollection builder, Action<TaskDefinitionOptions> updateOptions = null)
+            where TWorkerTask : IWorker
         {
-            builder.AddSingleton(ctx => ctx.ResolveTaskDefinitionBuilder(buildConfiguration).Build<TWorkerTask>(updateOptions));
+            builder.AddSingleton(ctx => ctx.GetRequiredService<TaskDefinitionBuilder>().Build<TWorkerTask>(updateOptions));
 
             builder.AddTransient(
                 ctx =>
                     new TaskToWorker
                     {
-                        TaskName = ctx.ResolveTaskDefinitionBuilder(buildConfiguration).Build<TWorkerTask>(updateOptions).Name,
+                        TaskName = ctx.GetRequiredService<TaskDefinitionBuilder>().Build<TWorkerTask>(updateOptions).Name,
                         TaskType = typeof(TWorkerTask)
                     }
             );
-        }
-
-        private static TaskDefinitionBuilder ResolveTaskDefinitionBuilder(
-            this IServiceProvider componentContext,
-            BuildConfiguration buildConfiguration
-        )
-        {
-            var builder = componentContext.GetRequiredService<TaskDefinitionBuilder>();
-
-            builder.BuildConfiguration = buildConfiguration ?? builder.BuildConfiguration;
-
-            return builder;
         }
     }
 }
