f6df31e4af9296d1e4eb33262402cf3c7013749c
bojan.malinic <bojan.malinic@codaxy.com>
2024-05-21 15:40:10 +0200

    Update serializer settings

--- src/ConductorSharp.Client/ConductorClient.cs
+++ src/ConductorSharp.Client/ConductorClient.cs
@@ -1,7 +1,15 @@
-﻿namespace ConductorSharp.Client.Generated
+﻿using ConductorSharp.Client.Util;
+
+namespace ConductorSharp.Client.Generated
 {
     public partial class ConductorClient
     {
         public HttpClient Client => _httpClient;
+
+        static partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings)
+        {
+            settings.Converters.Add(new JsonElementConverter());
+            settings.Converters.Add(new JsonNodeConverter());
+        }
     }
 }
