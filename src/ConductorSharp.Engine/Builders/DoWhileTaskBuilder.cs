a8db9e27b88b1085b603ba1f82974c01aa37d34c
Roel <roel.v.bergen@gmail.com>
2025-05-14 16:08:40 +0200

    Added the builder to support the do-while task type (#205)
    * Added do while task builder
    
    * - Changed line endings to LF
    - Updated Do while task to include value input
    - Moved loop condition from input to parameter in function
    
    ---------
    
    Co-authored-by: Roel Bergen, van <roel.vanbergen@vodafoneziggo.com>

---
+++ src/ConductorSharp.Engine/Builders/DoWhileTaskBuilder.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using ConductorSharp.Client.Generated;
+using ConductorSharp.Engine.Interface;
+using ConductorSharp.Engine.Model;
+using ConductorSharp.Engine.Util;
+using ConductorSharp.Engine.Util.Builders;
+
+namespace ConductorSharp.Engine.Builders
+{
+    /// <summary>
+    /// Extension methods to add a DO_WHILE loop with multiple inner tasks, based on BaseTaskBuilder.
+    /// </summary>
+    public static class DoWhileTaskExtensions
+    {
+        /// <summary>
+        /// Adds a DO_WHILE loop to the workflow definition.
+        /// </summary>
+        /// <typeparam name="TWorkflow">Workflow type</typeparam>
+        /// <param name="builder">Parent sequence builder</param>
+        /// <param name="reference">Expression selecting the workflow property holding the loop's reference name</param>
+        /// <param name="input">Expression creating the input</param>
+        /// <param name="loopCondition">Loop condition</param>
+        /// <param name="configureBody">Delegate to add tasks inside the loop</param>
+        public static ITaskOptionsBuilder AddTask<TWorkflow>(
+            this ITaskSequenceBuilder<TWorkflow> builder,
+            Expression<Func<TWorkflow, DoWhileTaskModel>> reference,
+            Expression<Func<TWorkflow, DoWhileInput>> input,
+            string loopCondition,
+            Action<ITaskSequenceBuilder<TWorkflow>> configureBody
+        )
+            where TWorkflow : ITypedWorkflow
+        {
+            // Extract expressions
+            var taskBuilder = new DoWhileTaskBuilder<TWorkflow>(reference.Body, input.Body, loopCondition, builder.BuildConfiguration);
+            // Register it with the outer sequence
+            builder.AddTaskBuilderToSequence(taskBuilder);
+            // Allow the caller to configure inner loop body tasks
+            configureBody(taskBuilder);
+            return taskBuilder;
+        }
+    }
+
+    /// <summary>
+    /// Builder for the DO_WHILE task, extending BaseTaskBuilder for consistent patterns.
+    /// </summary>
+    internal sealed class DoWhileTaskBuilder<TWorkflow> : BaseTaskBuilder<DoWhileInput, NoOutput>, ITaskSequenceBuilder<TWorkflow>
+        where TWorkflow : ITypedWorkflow
+    {
+        private readonly string _loopCondition;
+        private readonly DoWhileInput _doWhileInput;
+        private readonly List<WorkflowTask> _innerTasks = new();
+        public BuildContext BuildContext { get; } = new();
+        public BuildConfiguration BuildConfiguration { get; }
+        public WorkflowBuildItemRegistry WorkflowBuildRegistry { get; } = new();
+        public IEnumerable<ConfigurationProperty> ConfigurationProperties { get; } = new List<ConfigurationProperty>();
+
+        /// <inheritdoc />
+        public DoWhileTaskBuilder(Expression taskExpression, Expression inputExpression, string loopCondition, BuildConfiguration buildConfiguration)
+            : base(taskExpression, inputExpression, buildConfiguration)
+        {
+            _loopCondition = loopCondition;
+            BuildConfiguration = buildConfiguration;
+        }
+
+        /// <inheritdoc/>
+        public override WorkflowTask[] Build()
+        {
+            var refName = _taskRefferenceName;
+
+            if (_innerTasks.Any(t => t.WorkflowTaskType == WorkflowTaskType.DO_WHILE))
+            {
+                throw new InvalidOperationException("Nested DO_WHILE tasks are not allowed.");
+            }
+
+            var loopTask = new WorkflowTask
+            {
+                Name = refName,
+                TaskReferenceName = refName,
+                WorkflowTaskType = WorkflowTaskType.DO_WHILE,
+                Type = nameof(WorkflowTaskType.DO_WHILE),
+                InputParameters = _inputParameters.ToObject<IDictionary<string, object>>(),
+                LoopCondition = _loopCondition,
+                LoopOver = _innerTasks,
+            };
+            return [loopTask];
+        }
+
+        public void AddTaskBuilderToSequence(ITaskBuilder builder)
+        {
+            foreach (var task in builder.Build())
+            {
+                _innerTasks.Add(task);
+            }
+        }
+    }
+}
