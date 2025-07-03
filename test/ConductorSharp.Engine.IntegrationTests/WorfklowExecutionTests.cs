347357d53e168ee1023fa0eb65cfd408e6465ea7
bojan.malinic <bojan.malinic@codaxy.com>
2025-07-03 13:17:18 +0200

    Add initial setup for TestContainers

---
+++ test/ConductorSharp.Engine.IntegrationTests/WorfklowExecutionTests.cs
@@ -0,0 +1,8 @@
+namespace ConductorSharp.Engine.IntegrationTests
+{
+    public class WorfklowExecutionTests : IClassFixture<ConductorFixture>
+    {
+        [Fact]
+        public void Test1() { }
+    }
+}
